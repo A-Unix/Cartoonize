6d73a1c127b549f97f067547b8c559850523a952
Atul93 <54507991+A-Unix@users.noreply.github.com>
2024-03-18 19:38:25 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo -e "Checking if the script is running as root!"
+
+# Check if script is running as root user
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. Please use 'sudo' along with the command or login as root user."
+    sleep 1
+    echo "Saving Preferences for next try!"
+    sleep 7
+    exit 1
+fi
+
+# Check if Colorama has been already installed or not
+if python3 -c "import colorama" &>/dev/null; then
+    echo -e "\033[95mColorama has been already installed, We have initialized it for you :)\033[0m"
+    sleep 5
+else
+    echo -e "\033[91mColorama has not been installed. Installing it...\033[0m"
+    pip install colorama
+    echo -e "\033[95mDone, Colorama has been installed.\033[0m"
+    sleep 3
+fi
+
+
+echo -e "\033[93m\nUpdating your system, wait!\n\033[0m"
+sleep 0.5
+
+# Update system
+apt update
+
+# Install pip
+apt install python3-pip -y
+
+echo -e "\033[96m\nInstalling required dependencies, wait!\n\033[0m"
+sleep 0.5
+
+# Install requirements to run the script
+pip install opencv-python
+apt install figlet lolcat -y
+
+echo -e "\033[32m\nDone, run the 'main.py' file now!\n\033[0m"
+sleep 2
